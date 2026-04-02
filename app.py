9312d3da5606ac37763d0803dd0d06d3d6ae3e38
Sarra Labed <labedsar@gmail.com>
2026-04-02 02:00:13 +0100

    fix CI/CD pipeline

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ def home():
     return "DevOps CI/CD Working!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run (host="0.0.0.0", port=5000, debug=False)
     
         
